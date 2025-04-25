ffd7891af01bbf1446caeacffa64b19b50182981
muhammadaminusmonov <muhammadamincoder@gmail.com>
2025-04-25 19:46:27 +0500

    add generic api

--- src/api/blog/urls.py
+++ src/api/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import BlogView, BlogDetailView
+from .views import BlogView, BlogDetailView, BlogGenericView, BlogDetailGenericView, BlogGenericMixinsView, BlogDetailGenericMixinView
 
 urlpatterns = [
     path('', BlogView.as_view(), name='api_blog'),
     path('<int:pk>/', BlogDetailView.as_view(), name='api_blog_detail'),
+    path('generic/', BlogGenericView.as_view(), name='api_blog_generic'),
+    path('generic/<int:pk>/', BlogDetailGenericView.as_view(), name='api_blog_detail_generic'),
+    path('mixin/', BlogGenericMixinsView.as_view(), name='api_blog_mixin'),
+    path('mixin/<int:pk>/', BlogDetailGenericMixinView.as_view(), name='api_blog_detail_mixin'),
 ]
--- src/api/blog/views.py
+++ src/api/blog/views.py
@@ -1,5 +1,7 @@
+from django.core.serializers import get_serializer
 from django.shortcuts import get_object_or_404
-from rest_framework import status
+from rest_framework import status, mixins
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
@@ -51,3 +53,75 @@ class BlogDetailView(APIView):
         blog = get_object_or_404(Blog, pk=pk)
         blog.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# ------------------------------------------------------------
+
+
+class BlogGenericView(GenericAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+    def get(self, request):
+        blogs = self.get_queryset()
+        serializer = self.get_serializer(blogs, many=True)
+        return Response({'results': serializer.data})
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlogDetailGenericView(GenericAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+    def get(self, request, *args, **kwargs):
+        blog = self.get_object()
+        serializer = self.get_serializer(blog)
+        return Response({'results': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        blog = self.get_object()
+        serializer = self.get_serializer(blog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        blog = self.get_object()
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+class BlogGenericMixinsView(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class BlogDetailGenericMixinView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,GenericAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+# ----------------------------------------------------------------
+
